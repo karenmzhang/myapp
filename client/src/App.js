5f385cffee9171ef871ceb63e9ea0fa9e3ee4ecf
Karen Zhang <karen.michelle.zhang@gmail.com>
2019-01-06 00:23:38 -0500

    added codemirror

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Controlled as CodeMirror} from 'react-codemirror2';
+require('codemirror/mode/clike/clike');
 
 class App extends Component {
-      state = {
-        response: '',
+     state = {
         code: '',
-        responseToPost: '',};
-      
-      componentDidMount() {
-         this.callApi()
-            .then(res => this.setState({ response: res.express }))
-            .catch(err => console.log(err));
-      }
+        output: '',};
 
-      callApi = async () => {
-         const response = await fetch('/api/start');
-         const body = await response.json();
-         if (response.status !== 200) throw Error(body.message);
-         return body;
-      };
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/submit', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({code: this.state.code}),
+        });
+        const body = await response.text();
 
-      handleSubmit = async e => {
-         e.preventDefault();
-         const response = await fetch('/api/submit', {
-              method: 'POST',
-              headers: {'Content-Type': 'application/json',
-               },
-              body: JSON.stringify({code: this.state.code}),
-            });
-         const body = await response.text();
+        this.setState({output: body});
+    };
 
-         this.setState({responseToPost: body});
-      };
+    render() {
+	var options = {
+	    mode: 'text/x-java',
+	    lineNumbers: true
+	};
+	return (
+	    <div className="codemirror-box">
 
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-        <body>
-          <p>
-      <form onSubmit = {this.handleSubmit}>
-             <textarea rows = "5" cols = "60" name = "code"
-      value = {this.state.code}
-      onChange = {e=> this.setState({code: e.target.value}) }/>
-             <br/>
-             <input type="submit" value="Submit"/>
-          </form>
-          </p>
-      <p>
-         {this.state.responseToPost}
-      </p>
-
-        </body>
-      </div>
-    );
-  }
+	    <CodeMirror
+	    value={this.state.code}
+	    options={options}
+	    onBeforeChange={(editor, data, value) => {
+		this.setState({code:value});
+	    }}	
+	    onChange={(editor, data, value) => {
+	    }}
+	    />
+	    <form onSubmit={this.handleSubmit}>
+	    <input type="submit" value ="submit" id="submitButton"/>
+	    </form>
+	    <p>{this.state.output}</p>
+	    </div>
+	);
+    }
 }
 
 export default App;
