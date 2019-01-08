6830bdcff876f7a6d1c3c0d9a8ff4a1154bb9b6a
Karen Zhang <karen.michelle.zhang@gmail.com>
2019-01-08 01:30:41 -0500

    push to heroku try 2

--- models/sequelize.js
+++ models/sequelize.js
@@ -7,10 +7,7 @@ if (process.env.DATABASE_URL) {
     const sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
-    })
+    });
 } else {
     const sequelize = new Sequelize('testdb', 'karenzhang', '', {
 	host: 'localhost',
