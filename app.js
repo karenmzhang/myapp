929ecf434a64aa6ff406c312d2f11ec23f6454a3
Karen Zhang <karen.michelle.zhang@gmail.com>
2019-03-27 01:17:07 -0400

    aaaaahhhh still not working heroku

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use('/api/user', userRouter);
 app.use('/api/snapshot', snapshotRouter);
 app.use('/api/auth', authRouter.router);
 app.use('/api/runTests', testRouter);
-app.get('/*', (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
+app.get('*', (req, res) => {
+    let url = path.join(__dirname, 'client/build', 'index.html');
     if (!url.startsWith('/app/')) // we're on local windows
 	url = url.substring(1);
     res.sendFile(url);
